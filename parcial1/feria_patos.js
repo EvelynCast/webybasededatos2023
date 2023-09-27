90c0da50c6e30ecf8649c41ad0db7c934b46bfa0
EvelynCast <124410743+EvelynCast@users.noreply.github.com>
2023-09-26 19:43:11 -0700

    Proyecto parcial 1

--- parcial1/feria_patos.js
+++ parcial1/feria_patos.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
     var pato = "";
     var total_puntos = 0;
     var total_intentos = 0;
+    var contador=0;
+
 
     $('img').click(function (e){
         var estado = $(this).attr('data-estado')
@@ -12,7 +14,32 @@ $(document).ready(function(){
             console.log(total_puntos);
             $(this).attr('src', "proyecto/pato" + puntos + ".png")
             $(this).attr('data-estado', "1")
+            total_intentos++
+            $("#total_puntos").html(total_puntos)
+
         }
+        if(total_intentos >= 5){
+            alert("Has perdido el juego =(, paga otra ronda")
+            $('img').each(function(){
+                $(this).attr('data-estado', '1')
+            });
+            total_puntos = 0
+            total_intentos = 0
+            total_puntos++
+           
+        }
+
+        if(total_puntos >= 30){
+            alert("Felicidades, has ganado el juego =)")
+            $('img').each(function(){
+                $(this).attr('src','proyecto/pato.png')
+                $(this).attr('data-estado','0')
+            });
+            total_puntos=0;
+            return;
+        }
+
+      
 
     });
 });
