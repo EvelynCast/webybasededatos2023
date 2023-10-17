a3db31e03e321ddc493a622930750807710e572f
EvelynCast <124410743+EvelynCast@users.noreply.github.com>
2023-10-16 19:28:59 -0700

    Add files via upload

--- parcial2/home.php
+++ parcial2/home.php
@@ -3,60 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio</title>
+    <title>Catálogo de Alumnos</title>
     <link rel="stylesheet" href="css/bootstrap.css">
 </head>
 <body>
-    <?php include 'conexion.php'; ?>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Cetis 107</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="#">Inicio</a>
-            </li>
-            <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Menú
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="#">Consultar alumnos</a></li>
-                <li><a class="dropdown-item" href="#">Registrar alumno</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li>
-        </ul>
-        <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
-        </div>
-    </div>
-    </nav>
+    <?php include 'menu.php'; ?>
+
     <div class="container">
         <div class="row">
-            <div class="card">
-                <div class="card m-2">
-                    <h1>Sistema de gestión de control escolar</h1>
-                    <h3>CETIS 107</h3>
-                    <img src="imagenes/logocetis.png" alt="" width="20%" class="img-fluid">
-                </div>
+            <div class="card-12 text-center">
+                <h2>Bienvenido al sistema de gestión de alumnos</h2>
+                <img src="imagenes/logocetis.png" alt="">
             </div>
         </div>
     </div>
-    <footer class="text-center"><hr>
-        <span>Cetis 107 web App &copy; 2023</span>
+
+    <footer class="text-center">
+        <hr>
+        CETIS 107 web App &copy; 2023
     </footer>
     <script src="js/bootstrap.js"></script>
 </body>
